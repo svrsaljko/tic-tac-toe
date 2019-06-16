c198a49ffa029fc23fc49c31e000eeda5b00dc92
stipev <cupkou12@gmail.com>
2019-06-16 13:08:41 +0200

    basic design improve, added some functionalities

--- src/App.js
+++ src/App.js
@@ -3,13 +3,51 @@ import "./App.css";
 import Header from "./components/Header";
 import TicTacToe from "./components/TicTacToe";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <TicTacToe />
-    </div>
-  );
-}
+const EMPTY_BOARD = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+//const DISABLE_FIELD_LIST = [ false, false ,false ]
 
+class App extends React.Component {
+  state = {
+    board: EMPTY_BOARD,
+    turn: true
+    //  fieldLocker:
+  };
+
+  onFieldClick = e => {
+    const id = e.target.id;
+    //console.log(id);
+    let counter = 0;
+    let turn;
+    this.setState(state => {
+      const board = state.board.map(field => {
+        if (id == counter) {
+          if (state.turn === true) {
+            field = "X";
+            turn = false;
+          } else {
+            field = "0";
+            turn = true;
+          }
+        }
+
+        counter++;
+        return field;
+      });
+      return {
+        board,
+        turn
+      };
+    });
+  };
+
+  render() {
+    console.log(this.state.turn);
+    return (
+      <div className="App">
+        <Header />
+        <TicTacToe onFieldClick={this.onFieldClick} board={this.state.board} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,24 +1,43 @@
 import React from "react";
 
-export default function TicTacToe() {
+export default function TicTacToe(props) {
+  //console.log(props);
   return (
     <div className="TicTacToe">
-      <div className="PLayerContainer">Player1</div>
+      <div className="PLayerContainer">Player1 W D</div>
       <div className="BoardAndResetContainer">
         <button className="ResetButton">RESET</button>
         <div className="Board">
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
-          <div>X</div>
+          <button id="0" onClick={props.onFieldClick} className="Field">
+            {props.board[0]}
+          </button>
+          <button id="1" onClick={props.onFieldClick} className="Field">
+            {props.board[1]}
+          </button>
+          <button id="2" onClick={props.onFieldClick} className="Field">
+            {props.board[2]}
+          </button>
+          <button id="3" onClick={props.onFieldClick} className="Field">
+            {props.board[3]}
+          </button>
+          <button id="4" onClick={props.onFieldClick} className="Field">
+            {props.board[4]}
+          </button>
+          <button id="5" onClick={props.onFieldClick} className="Field">
+            {props.board[5]}
+          </button>
+          <button id="6" onClick={props.onFieldClick} className="Field">
+            {props.board[6]}
+          </button>
+          <button id="7" onClick={props.onFieldClick} className="Field">
+            {props.board[7]}
+          </button>
+          <button id="8" onClick={props.onFieldClick} className="Field">
+            {props.board[8]}
+          </button>
         </div>
       </div>
-      <div className="PLayerContainer">Player2</div>
+      <div className="PLayerContainer">Player2 W D</div>
     </div>
   );
 }
