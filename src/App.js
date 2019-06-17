421706e2234143e005b7ccecaa11f30c81acff19
stipev <cupkou12@gmail.com>
2019-06-17 15:18:37 +0200

    added winner by 2nd diagonal functionality, tested on N=2,3,4

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const EMPTY_BOARD = [
   "O",
   "X",
   "11",
-  "12",
+  "O",
   "13",
   "14",
   "X"
@@ -105,6 +105,22 @@ class App extends React.Component {
     //CHECK 1. DIAGONAL
     fieldChecker = 0;
     for (let i = N - N; i < N * N; i = i + N + 1) {
+      if (this.state.board[i] === fieldChar) {
+        fieldChecker++;
+      }
+    }
+    if (fieldChecker === N) {
+      console.log(fieldChar, ": is winner");
+      if (fieldChar === "X") {
+        return 0;
+      }
+      if (fieldChar === "O") {
+        return 1;
+      }
+    }
+    //CHECK 2. DIAGONAL
+    fieldChecker = 0;
+    for (let i = N - 1; i <= (N - 1) * N; i = i + N - 1) {
       if (this.state.board[i] === fieldChar) {
         fieldChecker++;
       }
